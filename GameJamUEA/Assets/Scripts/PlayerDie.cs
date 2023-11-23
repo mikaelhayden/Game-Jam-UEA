7fd38a34645ef396c630c2bb0e1b6a6a37ee47df
Waleson Melo <walesonmelo23@gmail.com>
2023-11-22 22:54:25 -0400

    Add Lifebar

--- GameJamUEA/Assets/Scripts/PlayerDie.cs
+++ GameJamUEA/Assets/Scripts/PlayerDie.cs
@@ -1,20 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class PlayerDie : MonoBehaviour
 {
     public FireArea area;
-    public int health;
+    public int health = 3;
     public bool isDead;
     private Animator anim;
- 
+    private List<RawImage> coracoes;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        RawImage[] rawImages = GetComponentsInChildren<RawImage>();
+        coracoes = rawImages.ToList();
     }
 
     // Update is called once per frame
@@ -31,10 +36,16 @@ public class PlayerDie : MonoBehaviour
         }
     }
 
-    public void dano() //Função para fazer o Player Morrer
+    public void dano() //Funï¿½ï¿½o para fazer o Player Morrer
     {
         health -= 1;
         area.Velocidade = 0.0f;
+
+        if (coracoes.Count != 0)
+        {
+            coracoes.Last<RawImage>().enabled = false;
+            coracoes.Remove(coracoes.Last<RawImage>());
+        }
 
         if (health <= 0)
         {
