fb09445baddb15c7b771cc7289934b946f4f2993
Waleson Melo <walesonmelo23@gmail.com>
2023-11-23 00:04:15 -0400

    Coleta de Animais adicionado, pontos adicionados

--- GameJamUEA/Assets/Scripts/DetectaAnimal.cs
+++ GameJamUEA/Assets/Scripts/DetectaAnimal.cs
@@ -1,15 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class DetectaAnimal : MonoBehaviour
 {
+
+    private TextMeshProUGUI pontos;
+    private int count;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pontos = FindObjectOfType<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
@@ -22,7 +29,9 @@ public class DetectaAnimal : MonoBehaviour
     {
         if (collider.tag == "Animal")
         {
+            count += 1;
             collider.gameObject.SetActive(false);
+            pontos.text = count.ToString();
         }
 
         Console.WriteLine("OnTriggerEnter");
